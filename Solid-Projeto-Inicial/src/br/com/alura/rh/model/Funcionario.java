1c605a72edcd9e8d5c5e4540cd11a611febc1dcf
Tboias <candidobr12@gmail.com>
2021-03-03 13:10:49 -0300

    NovaClasse para ReajusteSalarial

--- Solid-Projeto-Inicial/src/br/com/alura/rh/model/Funcionario.java
+++ Solid-Projeto-Inicial/src/br/com/alura/rh/model/Funcionario.java
@@ -1,18 +1,47 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 
-import br.com.alura.rh.ValidacaoException;
-
 public class Funcionario {
+
+	/**
+	 * Uma classe Coesa é uma classe onde cada um executa sua propriá tarefa, cada
+	 * classe deve ser responsável por fazer cada coisa , e deve executar sua tarefa
+	 * muito bem.
+	 * Uma classe Coesa faz bem uma única coisa Classes coesas não devem
+	 * ter várias responsabilidades
+	*/
+
+	/**
+	 * Classes não encapsuladas permitem violações de regras de negocio, além de
+	 * aumentarem o acoplamento
+	 * 
+	 * Getters e Setters somente não fazem encapsulamento, depende da gente de saber
+	 * utilizar da melhor forma, encapsulamento ajuda a gente usar corretamente o
+	 * uso de objetos
+	 * 
+	 * Getters e setters não são formas eficientes de aplicar encapsulamento
+	 * É interessante fornecer acesso apenas ao que é necessário em nossas classes 
+	 * O encapsulamento torna o uso das nossas classes mais fácil e intuitivo
+	*/
+
+	/**
+	 * Classes acopladas(juntas Ex: a = b.getPreço; ) geram fragilidade no código da
+	 * aplicação, o que dificulta sua manutenção. É impossível criar um sistema bom
+	 * sem um acoplamento, uma hora ou outra iremos usar uma classe que dependa da
+	 * outra, é só nós usarmos direito
+	 * 
+	 * Acoplamento é a dependência entre classes Acoplamento nem sempre é ruim, e
+	 * que é impossível criar um sistema sem nenhum acoplamento Devemos controlar o
+	 * nível de acoplamento na nossa aplicação (falaremos mais sobre isso)
+	*/
 
 	private String nome;
 	private String cpf;
 	private Cargo cargo;
 	private BigDecimal salario;
-	private LocalDate dataUltimoReajuste;
+	protected LocalDate dataUltimoReajuste;
 
 	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
 		this.nome = nome;
@@ -21,12 +50,8 @@ public class Funcionario {
 		this.salario = salario;
 	}
 
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.salario = novoSalario;
 		this.dataUltimoReajuste = LocalDate.now();
 	}
 
